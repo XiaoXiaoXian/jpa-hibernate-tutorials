7acc0532c29e9c8b3f8f7537379e4008271b2ec1
zhi_hb <zhi_hb@163.com>
2018-10-25 22:23:23 +0800

    修改数据库配置信息 添加format-sql选项 修改service.port端口为20081 添加测试方法 testInsert

--- jpa-embeddable-demo/src/test/java/com/example/jpa/JpaEmbeddableDemoApplicationTests.java
+++ jpa-embeddable-demo/src/test/java/com/example/jpa/JpaEmbeddableDemoApplicationTests.java
@@ -1,16 +1,53 @@
 package com.example.jpa;
 
+import com.example.jpa.model.Address;
+import com.example.jpa.model.Name;
+import com.example.jpa.model.User;
+import com.example.jpa.repository.UserRepository;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JpaEmbeddableDemoApplicationTests {
+	@Autowired
+	UserRepository userRepository;
+
+	@Test
+	public void testDIRepository( ){
+
+		assertNotNull( userRepository );
+	}
+
+	/**
+	* 应该尽量减少update的产生,方法是，先插入user,然后再插入其他的
+	* @param
+	* @return
+	* @throws
+	* @description :
+	*/
+	@Test
+	public void testInsert( ){
+		Address address = new Address("address lin1","address lin2","suzhou","0512","china","+81" );
+		Name name = new Name("zhi","ho","bin");
+		User user = new User();
+		user.setId(1L);
+		user.setEmail("zhihb@163.com");
+		user.setAddress(address);
+		user.setName(name);
+		userRepository.save( user );
+	}
 
 	@Test
 	public void contextLoads() {
+
+
 	}
 
 }
